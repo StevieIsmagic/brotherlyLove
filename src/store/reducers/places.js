c39ed92aad7ac044b03e45a047feeb62dd76e630
Colombianito66 <colombianito66@gmail.com>
2018-09-10 18:02:42 -0400

    init places reducer

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE
+} from './actionTypes';
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,7 +12,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case ADD_PLACE:
+      return {
+        ...state
+      };
     default:
       return state;
   }
