480da868fead6d376c3c9dfc2b41db78ecf5cc67
Colombianito66 <colombianito66@gmail.com>
2018-09-10 19:36:54 -0400

    add places reducer

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -14,7 +14,34 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PLACE:
       return {
-        ...state
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri: 'https://i.pinimg.com/236x/d3/61/cc/d361cc194b7cd8309d373cac8b8c54fe--fraternity-gifts-greek-crafts.jpg'
+          }
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key ===  action.placeKey;
+        })
+      };
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
       };
     default:
       return state;
