5b7314a3ac0d6016ef583fdfce15eec091cb5384
Colombianito66 <colombianito66@gmail.com>
2018-09-13 19:17:02 -0400

    connect redux to screens

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AnnouncementsScreen from './src/screens/Announcements/Announcements';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 // Register Screens
-Navigation.registerComponent('brotherlylove.AnnouncementsScreen', () => AnnouncementsScreen);
-Navigation.registerComponent('brotherlylove.AuthScreen', () => AuthScreen);
-Navigation.registerComponent('brotherlylove.SharePlaceScreen', () => SharePlaceScreen);
-Navigation.registerComponent('brotherlylove.FindPlaceScreen', () => FindPlaceScreen);
+Navigation.registerComponent('brotherlylove.AnnouncementsScreen', () => AnnouncementsScreen, store, Provider);
+Navigation.registerComponent('brotherlylove.AuthScreen', () => AuthScreen, store, Provider);
+Navigation.registerComponent('brotherlylove.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent('brotherlylove.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
 
 // Start App
 Navigation.startSingleScreenApp({
