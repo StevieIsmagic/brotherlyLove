3f9379667bb06e5caea0e83eaf1f79aa82e47a1a
Colombianito66 <colombianito66@gmail.com>
2018-09-13 18:57:59 -0400

    add tab icons

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { Navigation } from 'react-native-navigation';
 
+import AnnouncementsScreen from './src/screens/Announcements/Announcements';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
 // Register Screens
+Navigation.registerComponent('brotherlylove.AnnouncementsScreen', () => AnnouncementsScreen);
 Navigation.registerComponent('brotherlylove.AuthScreen', () => AuthScreen);
 Navigation.registerComponent('brotherlylove.SharePlaceScreen', () => SharePlaceScreen);
 Navigation.registerComponent('brotherlylove.FindPlaceScreen', () => FindPlaceScreen);
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,25 +1,35 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'brotherlylove.FindPlaceScreen',
-        label: 'Find Place',
-        title: 'Find Place'
-      },
-      {
-        screen: 'brotherlylove.SharePlaceScreen',
-        label: 'Share Place',
-        title: 'Share Place'
-      },
-      {
-        screen: 'brotherlylove.AnnouncementsScreen',
-        label: 'Announcements',
-        title: 'Announcements'
-      }
-    ]
-  });
+  Promise.all([
+    Icon.getImageSource('ios-map', 30),
+    Icon.getImageSource('ios-share-alt', 30),
+    Icon.getImageSource('ios-mail', 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'brotherlylove.FindPlaceScreen',
+          label: 'Find Brothers',
+          title: 'Find SigEp Brothers',
+          icon: sources[0]
+        },
+        {
+          screen: 'brotherlylove.SharePlaceScreen',
+          label: 'Share Place',
+          title: 'Share Your Location w/ Brothers',
+          icon: sources[1]
+        },
+        {
+          screen: 'brotherlylove.AnnouncementsScreen',
+          label: 'Announcements',
+          title: 'Announcements',
+          icon: sources[2]
+        }
+      ]
+    });
+  })
 }
-
-export default startTabs;
+  
+  export default startTabs;
