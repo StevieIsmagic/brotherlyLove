85f0af120ce60f85cd635666559dabf9ac2a5391
Colombianito66 <colombianito66@gmail.com>
2018-09-15 16:52:56 -0400

    add MainText reusable Styling Component

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -13,14 +14,18 @@ class AuthScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <HeadingText> Please Log In </HeadingText>
-        <Button title="Switch to Login" />
-        <View style={styles.inputContainer}>
-          <DefaultInput placeholder='Your Email Address' style={styles.input} />
-          <DefaultInput placeholder='Your Password' style={styles.input} />
-          <DefaultInput placeholder='Reconfirm Password' style={styles.input} />
-        </View>
-        <Button title="Submit" onPress={this.loginHandler}/>
+        <ImageBackground source={}>
+          <MainText>
+            <HeadingText> Please Log In </HeadingText>
+          </MainText>
+          <Button title="Switch to Login" />
+          <View style={styles.inputContainer}>
+            <DefaultInput placeholder='Your Email Address' style={styles.input} />
+            <DefaultInput placeholder='Your Password' style={styles.input} />
+            <DefaultInput placeholder='Reconfirm Password' style={styles.input} />
+          </View>
+          <Button title="Submit" onPress={this.loginHandler}/>
+        </ImageBackground>
       </View>
     );
   }
