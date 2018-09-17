95bf743acb16ff2be1cc7dfa60f647477dd82290
Colombianito66 <colombianito66@gmail.com>
2018-09-16 21:13:20 -0400

    update responsiveness

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -9,6 +9,30 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 import backgroundImage from '../../assets/man.png';
 
 class AuthScreen extends Component {
+  state = {
+    responsiveStyles: {
+      pwContainerDirection: 'column',
+      pwContainerJustifyContent: 'flex-start',
+      pwWrapperWidth: '100%'
+
+    }
+
+  };
+
+  constructor(props){
+    super(props);
+
+    Dimensions.addEventListener('change', dimensions => {
+      this.setState({
+        responsiveStyles: {
+          pwContainerDirection: Dimensions.get('window').height > 500 ? 'column' : 'row',
+          pwContainerJustifyContent: Dimensions.get('window').height > 500 ? 'flex-start' : 'space-between',
+          pwWrapperWidth: Dimensions.get('window').height > 500 ? '100%' : '45%'
+        }
+      })
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   }
@@ -30,12 +54,28 @@ class AuthScreen extends Component {
           {headingText}
           <ButtonWithBackground color='#53c294' onPress={() => alert('Login')}>Switch To Login</ButtonWithBackground>
           <View style={styles.inputContainer}>
-            <DefaultInput placeholder='Your Email Address' style={styles.input} />
-            <View style={styles.passwordContainer}>
-              <View style={styles.passwordWrapper}>
+            <DefaultInput 
+              placeholder='Your Email Address' 
+              style={styles.input} 
+            />
+            <View 
+              style={{
+                flexDirection: this.state.responsiveStyles.pwContainerDirection,
+                justifyContent: this.state.responsiveStyles.pwContainerJustifyContent
+              }}
+            >
+              <View 
+                style={{
+                  width: this.state.responsiveStyles.pwWrapperWidth
+                }}
+              >
                 <DefaultInput placeholder='Your Password' style={styles.input} />
               </View>
-              <View style={styles.passwordWrapper}>
+              <View 
+                style={{
+                  width: this.state.responsiveStyles.pwWrapperWidth
+                }}
+              >
                 <DefaultInput placeholder='Reconfirm Password' style={styles.input} />
               </View>
             </View>
