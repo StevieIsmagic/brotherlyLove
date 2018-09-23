93a181205f347f1ec959383ee6fdc5a18012e057
Colombianito66 <colombianito66@gmail.com>
2018-09-22 23:21:20 -0400

    add map marker onPress

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -11,19 +11,43 @@ class PickLocation extends Component {
       longitudeDelta: 
         Dimensions.get('window').width / 
         Dimensions.get('window').height * 0.0122
-
-    }
+    },
+    locationChosen: false
   };
 
+  picklLocationHandler = event => {
+    const coordinates = event.nativeEvent.coordinate;
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude
+        },
+        locationChosen: true
+      }
+    })
+  }
+
   render() {
+    let marker = null;
+
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+    }
+
     return (
       <View style={styles.container}>
         
          <MapView 
           // provider={PROVIDER_GOOGLE}
           initialRegion={this.state.focusedLocation}
+          region={this.state.focusedLocation}
+          onPress={this.picklLocationHandler}
           style={styles.map}
-         />
+         >
+          {marker}
+         </MapView>
 
         <View style={styles.button}>
           <Button title='Locate Me' onPress={() => alert('you pressed the button!')}/>
