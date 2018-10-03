03a459959262ae33a6097908d4eb202ff6591f52
Colombianito66 <colombianito66@gmail.com>
2018-10-03 14:34:23 -0400

    test babel-presets

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,13 +1,30 @@
 import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
+import { FIREBASE_ADDPLACE } from 'react-native-dotenv';
 
 // ACTION CREATORS > RETURN AN OBJECT
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch(FIREBASE_ADDPLACE, {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log("PARSED RESPONSE", parsedRes);
+    });
   };
+
+  // return {
+  //   type: ADD_PLACE,
+  //   placeName: placeName,
+  //   location: location,
+  //   image: image
+  // };
 };
 export const deletePlace = (key) => {
   return {
