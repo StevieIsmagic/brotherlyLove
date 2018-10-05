a93249ede0266e3e7a8af84e92ce9d2a04567fa4
Colombianito66 <colombianito66@gmail.com>
2018-10-04 20:26:54 -0400

    debug unfilfulled promise in places action creator

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,11 @@
 import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
-import { FIREBASE_ADDPLACE, FIREBASE_ADDIMAGE } from 'react-native-dotenv';
+import { FIREBASE_ADDPLACE, FIREBASE_ADDIMAGE_FX } from 'react-native-dotenv';
 
 // ACTION CREATORS > RETURN AN OBJECT
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    // Upload Image
-    fetch(FIREBASE_ADDIMAGE, {
+    // Upload Image to Firebase Storage via Firebase f(x)
+    fetch(FIREBASE_ADDIMAGE_FX, {
       method: "POST",
       body: JSON.stringify({
         image: image.base64
@@ -14,8 +14,7 @@ export const addPlace = (placeName, location, image) => {
     .catch(err => console.log("Places Error: ", err))
     .then(res => {
       console.log("RESSSSSAAAA: ", res.json())
-      // res.json()
-      
+      res.json()
     })
     .then(parsedRes => {
       console.log("PARSED RES FIRST PROMISE: ", parsedRes);
@@ -25,26 +24,19 @@ export const addPlace = (placeName, location, image) => {
         location: location,
         image: parsedRes.imageUrl
       };
-      return fetch(FIREBASE_ADDPLACE, {
-          method: "POST",
-          body: JSON.stringify(placeData)
-      })
     })
-    .catch(err => console.log("SECOND PROMISE ERR: ", err))
-    .then(res => res.json())
-    .then(parsedRes => {
-      console.log("PARSED RESPONSE: ", parsedRes);
-    });
+
+        // return fetch(FIREBASE_ADDPLACE, {
+        //     method: "POST",
+        //     body: JSON.stringify(placeData)
+        // })
+        // .catch(err => console.log("SECOND PROMISE ERR: ", err))
+        // .then(res => res.json())
+        // .then(parsedRes => {
+        //   console.log("SECOND PROMISE PARSED RESPONSE: ", parsedRes);
+        // });
   };
 };
-
-
-  // return {
-  //   type: ADD_PLACE,
-  //   placeName: placeName,
-  //   location: location,
-  //   image: image
-  // };
 
 export const deletePlace = (key) => {
   return {
